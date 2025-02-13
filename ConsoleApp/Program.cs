2618267fd95015c91c06844de7ebf39382a7dc2d
Ayesha-Javaidd <ayeshajavaid5005@gmail.com>
2025-02-13 22:05:09 +0500

    EMS finalizing

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using EmployeeLibrary;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("1) Add Employees ");
+            Console.WriteLine("2) View all Employees ");
+            Console.WriteLine("3) Search Employees by Name ");
+            Console.WriteLine("4) Sercah Employees by salary range ");
+            Console.WriteLine("5) Exit ");
+            int choice;
+            Console.WriteLine("Enter your choice : ");
+            choice = int.Parse(Console.ReadLine());
+            EmployeeManager mgr = new EmployeeManager();
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter name :");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Enter address :");
+                    string address = Console.ReadLine();
+                    Console.WriteLine("Enter salary :");
+                    float salary = float.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter tax :");
+                    float tax = float.Parse(Console.ReadLine());
+
+                    Employee emp = new Employee(name, address, salary, tax);
+                    mgr.Save(emp);
+                    Console.WriteLine("Employee saved successfully!");
+                    break;
+                case 2:
+                    List<Employee> list = new List<Employee>();
+                    list = mgr.ReadAll();
+                    if (list == null)
+                    {
+                        Console.WriteLine("No employees found ");
+                    }
+                    else
+                    {
+                        foreach (Employee empl in list)
+                        {
+                            Console.WriteLine(empl);
+                        }
+                    }
+                    break;
+
+                case 3:
+                    Console.WriteLine("Enter name :");
+                    string searchname = Console.ReadLine();
+                    Employee namedEmp = new Employee();
+
+                    List<Employee> employees = new List<Employee>();
+                    employees = mgr.SearchEmployeebyName(searchname);
+
+                    if (employees == null)
+                    {
+                        Console.WriteLine("No employees found ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Employee details: ");
+                        foreach (Employee empl in employees)
+                        {
+                            Console.WriteLine(empl);
+                        }
+                    }
+                    break;
+
+                case 4:
+                    Console.WriteLine("Enter lower range : ");
+                    float lower = float.Parse( Console.ReadLine());
+
+                    Console.WriteLine("Enter upper range : ");
+                    float upper = float.Parse(Console.ReadLine());
+
+                    List<Employee> salemployees = new List<Employee>();
+                    salemployees = mgr.SearchEmployeebyRange(lower, upper);
+
+                    if (salemployees == null)
+                    {
+                        Console.WriteLine("No employees found ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Employee details: ");
+                        foreach (Employee empl in salemployees)
+                        {
+                            Console.WriteLine(empl);
+                        }
+                    }
+                    break;
+
+                case 5:
+                    Console.WriteLine("Exiting the program !");
+                    break;
+
+                default:
+                    Console.WriteLine("Choice is invalid, Try again !");
+                    break;
+            }
+        }
+    }
+}
---
+++ EmployeeLibrary/Class1.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections.Generic;
+using System.IO;
+namespace EmployeeLibrary
+{
+    public class Employee
+    {
+        String name;
+        String address;
+        float salary;
+        float tax;
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public String Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public float Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+        public float Tax
+        {
+            get { return tax; }
+            set { tax = value; }
+        }
+
+        public override String ToString()
+        {
+            return $"Name : {name} , Address : {address}, Salary : {salary}, Tax : {tax}";
+        }
+
+        public Employee()
+        {
+        }
+
+        public Employee(string name, string address, float salary, float tax)
+        {
+            this.name = name;
+            this.address = address;
+            this.salary = salary;
+            this.tax = tax;
+        }
+    }
+
+
+    public class EmployeeManager
+    {
+        public void Save(Employee emp)
+        {         
+            FileStream fin = new FileStream("input.txt", FileMode.Append);
+            StreamWriter fout = new StreamWriter(fin);
+           
+            fout.WriteLine($"{emp.Name},{emp.Address},{emp.Salary},{emp.Tax}");
+            fout.Close();
+            fin.Close();
+        }
+        public List<Employee> ReadAll()
+        {
+            List<Employee> list = new List<Employee>();
+            FileStream fin = new FileStream("input.txt", FileMode.Open);
+            StreamReader fout = new StreamReader(fin);
+
+            if (!File.Exists("input.txt"))
+            {
+                Console.WriteLine("File not found");
+                return list;
+            }
+
+            String line;
+            while((line = fout.ReadLine()) != null)
+            {
+                String[] data = line.Split(',');
+                if (data.Length == 4)
+                {
+                    Employee emp = new Employee(data[0], data[1], float.Parse(data[2]), float.Parse(data[3]));
+                    list.Add(emp);
+                }
+            }
+            fin.Close() ;
+            fout.Close();
+            return list;
+        }
+
+        public List<Employee> SearchEmployeebyName(String name)
+        {
+            if (!File.Exists("input.txt"))
+                return null;
+
+            FileStream fin = new FileStream("input.txt", FileMode.Open);
+            StreamReader fout = new StreamReader(fin);
+            String line;
+            List<Employee> list = new List<Employee>();
+            while ((line = fout.ReadLine()) != null)
+            {
+                String[] data = line.Split(',');
+                if (data[0] == name)
+                {
+                    Employee employee = new Employee(data[0], data[1], float.Parse(data[2]), float.Parse(data[3]));
+                   list.Add((employee));
+                }
+            }
+            fin.Close();
+            fout.Close();
+
+            return list;
+        }
+
+
+        public List<Employee> SearchEmployeebyRange(float lower, float upper)
+        {
+            if (!File.Exists("input.txt"))
+                return null;
+
+            FileStream fin = new FileStream("input.txt", FileMode.Open);
+            StreamReader fout = new StreamReader(fin);
+            String line;
+            List<Employee> list = new List<Employee>();
+            while ((line = fout.ReadLine()) != null)
+            {
+                String[] data = line.Split(',');
+                if (int.Parse(data[2]) >= lower && int.Parse(data[2]) <= upper)
+                {
+                    Employee emp = new Employee(data[0], data[1], float.Parse(data[2]), float.Parse(data[3]));
+                    list.Add(emp);
+                }
+            }
+            return list;
+            fin.Close();
+            fout.Close();
+        }
+    }
+}
